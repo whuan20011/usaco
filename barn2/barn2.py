0b8568ff54dfc1f59a8c857c6381bbb3b9155613
whuan20011 <whuan20011@gmail.com>
2019-04-11 05:35:34 +0000

    no spaces between >> and fout give meaningful names use zip to get the spaces use sorted(list, reverse=True) to make a list from largest to smallest

--- barn2/barn2.py
+++ barn2/barn2.py
@@ -8,21 +8,16 @@ def solution():
     fout = open("barn1.out", "w")
     max_purchase, total_barns, occupied = map(int, fin.readline().strip().split())
     if max_purchase >= occupied:
-        print >> fout, occupied
+        print >>fout, occupied
         return
-    arr = []
+    gates = []
     for gate in fin.readlines():
-        arr.append(int(gate.strip()))
-    arr = sorted(arr)
-    brr = []
-    for i in range(1, len(arr)):
-        brr.append(arr[i] - arr[i - 1])
-    brr = sorted(brr)
-    brr = brr[:: -1]
-    count = 0
-    for j in range(max_purchase - 1):
-        count += brr[j]
-    print >> fout, arr[-1] - arr[0] + 1 - count + max_purchase - 1
+        gates.append(int(gate.strip()))
+    gates = sorted(gates)
+    spaces = [cur - pre for cur, pre in zip(gates[1:], gates)]
+    spaces = sorted(spaces, reverse=True)
+    count = sum(spaces[:max_purchase - 1])
+    print >>fout, gates[-1] - gates[0] + 1 - count + max_purchase - 1
 if __name__ == "__main__":
     solution()
     
