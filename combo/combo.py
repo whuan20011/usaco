d2a7adc8957ad7fbeffe2ae8533e7da02da16f48
whuan20011 <whuan20011@gmail.com>
2019-04-15 04:38:42 +0000

    to move num one digit to the left, then we can use mode, which is(num - 1 +1/-1/+2/-2) % N + 1 use map(lambda x: x + 1, arr) function to get rid of repitation

--- combo/combo.py
+++ combo/combo.py
@@ -7,19 +7,16 @@ def solution():
     fin = open("combo.in", "r")
     fout = open("combo.out", "w")
     N = int(fin.readline().strip())
-    a1, a2, a3 = map(int, fin.readline().strip().split())
-    b1, b2, b3 = map(int, fin.readline().strip().split())
+    farmers = map(int, fin.readline().strip().split())
+    masters = map(int, fin.readline().strip().split())
     if N >= 5:
-        farmer_dail1 = dail(a1, N)
-        farmer_dail2 = dail(a2, N)
-        farmer_dail3 = dail(a3, N)
-        master_dail1 = dail(b1, N)
-        master_dail2 = dail(b2, N)
-        master_dail3 = dail(b3, N)
-        count1 = same_number(farmer_dail1, master_dail1)
-        count2 = same_number(farmer_dail2, master_dail2)
-        count3 = same_number(farmer_dail3, master_dail3)
-        print >>fout, 250 - count1 * count2 * count3
+        farmers_dail = map(lambda x: dail(x, N), farmers)
+        masters_dail = map(lambda x: dail(x, N), masters)
+        count = map(lambda dic1, dic2: same_number(dic1, dic2), farmers_dail, masters_dail)
+        res = 1
+        for i in count:
+            res *= i
+        print >>fout, 250 - res
     else:
         print >>fout, N * N * N
 def dail(num, N):
@@ -27,8 +24,8 @@ def dail(num, N):
     dic[(num -1 - 1 + N) % N + 1] = True
     dic[(num - 1 - 2 + N) % N + 1] = True
     dic[num] = True
-    dic[(num + 1 + 1) % N - 1] = True
-    dic[(num + 1 + 2) % N - 1] = True
+    dic[(num - 1 + 1) % N + 1] = True
+    dic[(num - 1 + 2) % N + 1] = True
     return dic
 def same_number(dic1, dic2):
     count = 0
