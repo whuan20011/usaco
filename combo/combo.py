62edd0e6b5beb1ebee3673da5078ce1e470b7e53
whuan20011 <whuan20011@gmail.com>
2019-04-15 04:07:01 +0000

    changed dic in dail function

--- combo/combo.py
+++ combo/combo.py
@@ -10,36 +10,25 @@ def solution():
     a1, a2, a3 = map(int, fin.readline().strip().split())
     b1, b2, b3 = map(int, fin.readline().strip().split())
     if N >= 5:
-        farmer_dail1 = dail(a1, N, {})
-        farmer_dail2 = dail(a2, N, {})
-        farmer_dail3 = dail(a3, N, {})
-        master_dail1 = dail(b1, N, {})
-        master_dail2 = dail(b2, N, {})
-        master_dail3 = dail(b3, N, {})
+        farmer_dail1 = dail(a1, N)
+        farmer_dail2 = dail(a2, N)
+        farmer_dail3 = dail(a3, N)
+        master_dail1 = dail(b1, N)
+        master_dail2 = dail(b2, N)
+        master_dail3 = dail(b3, N)
         count1 = same_number(farmer_dail1, master_dail1)
         count2 = same_number(farmer_dail2, master_dail2)
         count3 = same_number(farmer_dail3, master_dail3)
         print >>fout, 250 - count1 * count2 * count3
     else:
         print >>fout, N * N * N
-def dail(num, N, dic):
-    if num - 1 > 0:
-        dic[num - 1] = True
-    else:
-        dic[num - 1 + N] = True
-    if num - 2 > 0:
-        dic[num - 2] = True
-    else:
-        dic[num - 2 + N] = True
+def dail(num, N):
+    dic = {}
+    dic[(num -1 - 1 + N) % N + 1] = True
+    dic[(num - 1 - 2 + N) % N + 1] = True
     dic[num] = True
-    if num + 1 > N:
-        dic[(num + 1) % N] = True
-    else:
-        dic[num + 1] = True
-    if num + 2 > N:
-        dic[(num + 2) % N] = True
-    else:
-        dic[num + 2] = True
+    dic[(num + 1 + 1) % N - 1] = True
+    dic[(num + 1 + 2) % N - 1] = True
     return dic
 def same_number(dic1, dic2):
     count = 0
