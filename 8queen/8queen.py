8ceb110baf082e0b4da70d76399dc68b8f5ceaa4
whuan2001 <whuan20011@gmail.com>
2019-05-04 06:05:31 +0000

    8queen It is a recursive problem, need to be more abstract. do more functions!

--- 8queen/8queen.py
+++ 8queen/8queen.py
@@ -8,37 +8,39 @@ def queen8(num, board):
             print board[c]
         return
     else:
-        temp = 8 - num
-        for j in range(0, 8):
-            tag = 0
-            for i in range(0, temp):
-                if board[i][j] == 1:
+        search_accessible_place(num, board)
+def search_accessible_place(num, board):
+    temp = 8 - num
+    for j in range(0, 8):
+        tag = 0
+        for i in range(0, temp):
+            if board[i][j] == 1:
+                tag = 1
+                break
+        if tag == 0:
+            row_to_leftdown = temp
+            col_to_leftup = j
+            while row_to_leftdown >= 0 and col_to_leftup >= 0:
+                if board[row_to_leftdown][col_to_leftup] == 0:
+                    row_to_leftdown -= 1
+                    col_to_leftup -= 1
+                else:
                     tag = 1
                     break
-            if tag == 0:
-                p = temp
-                q = j
-                while p >= 0 and q >= 0:
-                    if board[p][q] == 0:
-                        p -= 1
-                        q -= 1
-                    else:
-                        tag = 1
-                        break
-            if tag == 0:
-                u = temp
-                k = j
-                while u >= 0 and k <= 7:
-                    if board[u][k] == 0:
-                        u -= 1
-                        k += 1
-                    else:
-                        tag = 1
-                        break
-            if tag == 0:
-                new_board = copy.deepcopy(board)
-                new_board[temp][j] = 1
-                queen8(num - 1, new_board)
+        if tag == 0:
+            row_to_leftup = temp
+            col_to_leftup = j
+            while row_to_leftup >= 0 and col_to_leftup <= 7:
+                if board[row_to_leftup][col_to_leftup] == 0:
+                    row_to_leftup -= 1
+                    col_to_leftup += 1
+                else:
+                    tag = 1
+                    break
+        if tag == 0:
+            new_board = copy.deepcopy(board)
+            new_board[temp][j] = 1
+            queen8(num - 1, new_board)
 board = []
 for h in range(0, 8):
     arr = []
