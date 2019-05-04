c7a7525e7f135442118a5cbcef5649b572f557d7
whuan20011 <49110264+whuan20011@users.noreply.github.com>
2019-05-04 12:05:34 -0700

    watch he position of tag, under the first loop, not at a global space (#8)
    * watch he position of tag, under the first loop, not at a global space
    learn how to debug, how to check a recursion by printing num to check where the recursion stoped, why it was stopped
    It was stopped because the programme thought it was a leaf node.
    why it was a leaf node, because tag == 1, the loop did not do anything. The recursion line(queen8(num - 1, new_board))
    was not excuted, so it was thought as a leaf node and go back to the upper level.
    learn to use import copy at the begging, because we need to use new_board as a copy to show different board.
    For this problem, it has 8 new_boards at most.
    
    * 8queen
    It is a recursive problem, need to be more abstract. do more functions!
    
    * 8queen
    make the three directions in a function.
    
    * 8queen
    changed the function name

---
+++ 8queen/8queen.py
@@ -0,0 +1,46 @@
+import copy
+def queen8(num, board):
+    if num == 0:
+        global res
+        res += 1
+        print "------------------------------------------------"
+        for c in range(8):
+            print board[c]
+        return
+    else:
+        row = 8 - num
+        for col in range(0, 8):
+            if can_put_queen(row, col, board):
+                new_board = copy.deepcopy(board)
+                new_board[row][col] = 1
+                queen8(num - 1, new_board)
+def can_put_queen(row, col, board):
+    for i in range(0, row):
+        if board[i][col] == 1:
+            return False
+    row_to_leftdown = row
+    col_to_leftup = col
+    while row_to_leftdown >= 0 and col_to_leftup >= 0:
+        if board[row_to_leftdown][col_to_leftup] == 0:
+            row_to_leftdown -= 1
+            col_to_leftup -= 1
+        else:
+            return False
+    row_to_leftup = row
+    col_to_leftup = col
+    while row_to_leftup >= 0 and col_to_leftup <= 7:
+        if board[row_to_leftup][col_to_leftup] == 0:
+            row_to_leftup -= 1
+            col_to_leftup += 1
+        else:
+            return False
+    return True
+board = []
+for h in range(0, 8):
+    arr = []
+    for w in range(0, 8):
+        arr.append(0)
+    board.append(arr)
+res = 0
+queen8(8, board)
+print res
