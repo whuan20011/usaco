87362595b4b6cbe2dd326e982f82b87ceefa0dc1
whuan2001 <whuan20011@gmail.com>
2019-05-12 22:57:44 +0000

    sprime Algorithm: dfs thinking in a reversed direction

--- sprime/sprime.py
+++ sprime/sprime.py
@@ -9,23 +9,22 @@ def solution():
     fout = open("sprime.out", "w")
     N = int(fin.readline().strip())
     primes = []
-    for res in creat_primes(N, N, primes):
+    for res in create_primes(N, N, primes):
         print >>fout, res
-def creat_primes(N, num, primes):
+def create_primes(N, num, primes):
     if num == 0:
-        return primes[N - 1]
+        return primes
     else:
-        cur_digits_primes = []
+        cur_digit_primes = []
         if num == N:
-            cur_digits_primes = [2, 3, 5, 7]
-            primes.append(cur_digits_primes)
+            primes = [2, 3, 5, 7]
         else:
-            for p in primes[-1]:
+            for p in primes:
                 for q in (1, 3, 5, 7, 9):
                     if is_prime(p * 10 + q):
-                        cur_digits_primes.append(p * 10 + q)
-            primes.append(cur_digits_primes)
-        return creat_primes(N, num - 1, primes)
+                        cur_digit_primes.append(p * 10 + q)
+            primes = cur_digit_primes
+        return create_primes(N, num - 1, primes)
 def is_prime(a):
     if a == 2:
         return True
@@ -35,4 +34,4 @@ def is_prime(a):
     return True
 if __name__ == "__main__":
     solution()
-        
+    
