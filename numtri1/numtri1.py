19787ee74723347b579e05f559550b1b9964b4e1
whuan2001 <whuan20011@gmail.com>
2019-05-12 23:21:18 +0000

    numtri Algorithm: dfs thinking in a reversed direction using left and right to represent a node in a tree watch out the arguments in the function and make it as least as you can

--- numtri1/numtri1.py
+++ numtri1/numtri1.py
@@ -14,19 +14,18 @@ def solution():
         arr = map(int, line.strip().split())
         numtri.append(arr)
     dic = {}
-    res = max_sum(numtri, row, numtri[0][0], (0, 0), dic)
+    res = max_sum(numtri, row, (0, 0), dic)
     print >>fout, res
-def max_sum(numtri, row, cur_num, cur_index, dic):
+def max_sum(numtri, row, cur_index, dic):
+    cur_num = numtri[cur_index[0]][cur_index[1]]
     if cur_index in dic:
         return dic[cur_index]
     if cur_index[0] == row - 1:
         return cur_num
-    cur_num1 = numtri[cur_index[0] + 1][cur_index[1]]
-    cur_index1 = (cur_index[0] + 1, cur_index[1])
-    cur_num2 = numtri[cur_index[0] + 1][cur_index[1] + 1]
-    cur_index2 = (cur_index[0] + 1, cur_index[1] + 1)
-    res = max(cur_num + max_sum(numtri, row, cur_num1, cur_index1, dic),
-              cur_num + max_sum(numtri, row, cur_num2, cur_index2, dic))
+    left_index = (cur_index[0] + 1, cur_index[1])
+    right_index = (cur_index[0] + 1, cur_index[1] + 1)
+    res = max(cur_num + max_sum(numtri, row, left_index, dic),
+              cur_num + max_sum(numtri, row, right_index, dic))
     dic[cur_index] = res
     return res
 if __name__ == "__main__":
