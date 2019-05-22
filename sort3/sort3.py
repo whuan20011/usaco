ef2c5d6b1cb178eff78dd2d8af7f6cbd98967545
whuan2001 <whuan20011@gmail.com>
2019-05-22 22:02:30 +0000

    sort3 Algorithm: list change: meaningful names

--- sort3/sort3.py
+++ sort3/sort3.py
@@ -7,34 +7,34 @@ def solution():
     fin = open("sort3.in", "r")
     fout = open("sort3.out", "w")
     num = int(fin.readline().strip())
-    arr = []
+    numbers = []
     for line in fin.readlines():
-        arr.append(int(line.strip()))
+        numbers.append(int(line.strip()))
     count1 = 0
     count2 = 0
     count3 = 0
     for i in range(num):
-        if arr[i] == 1:
+        if numbers[i] == 1:
             count1 += 1
-        elif arr[i] == 2:
+        elif numbers[i] == 2:
             count2 += 1
-        elif arr[i] == 3:
+        elif numbers[i] == 3:
             count3 += 1
     temp = []
-    tag = 0
+    exchange_times = 0
     for j in range(num - 1, num - 1 - count3, -1):
-        if arr[j] != 3:
-            tag += 1
-            temp.append(arr[j])
+        if numbers[j] != 3:
+            exchange_times += 1
+            temp.append(numbers[j])
     temp = sorted(temp)
     h = 0
     for k in range(count1 + count2):
-        if arr[k] == 3:
-            arr[k] = temp[h]
+        if numbers[k] == 3:
+            numbers[k] = temp[h]
             h += 1
     for p in range(count1):
-        if arr[p] != 1:
-            tag += 1
-    print >>fout, tag
+        if numbers[p] != 1:
+            exchange_times += 1
+    print >>fout, exchange_times
 if __name__ == "__main__":
     solution()
