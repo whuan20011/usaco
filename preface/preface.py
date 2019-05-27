b49b4755d35a50c1888097b3d4428a75088ff166
whuan2001 <whuan20011@gmail.com>
2019-05-27 05:46:33 +0000

    preface Algorithm: changed dfs to while loop use string instead of list Important: count the characters in a string by using counter import collections res = collections.Counter(string)

--- preface/preface.py
+++ preface/preface.py
@@ -3,53 +3,46 @@ ID: whuan2001
 LANG: PYTHON2
 TASK: preface
 """
+import collections
 import sys   
 sys.setrecursionlimit(4000)
 def solution():
     fin = open("preface.in", "r")
     fout = open("preface.out", "w")
     N = int(fin.readline().strip())
-    res = {}
     dic = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
-    roman_numerals(N, 1, dic, res)
+    num = 1
+    all_characters = ""
+    while num <= N:    
+        t = -1
+        a = num
+        while num != 0:
+            m = num % 10
+            num /= 10
+            t += 1
+            if m == 1:
+                all_characters += dic[pow(10, t)]
+            if m == 2:
+                all_characters += dic[pow(10, t)] + dic[pow(10, t)]
+            if m == 3:
+                all_characters += dic[pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)]
+            if m == 4:
+                all_characters += dic[pow(10, t)] + dic[5 * pow(10, t)]
+            if m == 5:
+                all_characters += dic[5 * pow(10, t)]
+            if m == 6:
+                all_characters += dic[5 * pow(10, t)] + dic[pow(10, t)]
+            if m == 7:
+                all_characters += dic[5 * pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)]
+            if m == 8:
+                all_characters += dic[5 * pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)]
+            if m == 9:
+                all_characters += dic[pow(10, t)] + dic[pow(10, (t + 1))]
+        num = a + 1
+    res = collections.Counter(all_characters)
     for char in "IVXLCDM":
         if char in res:
             print >>fout, char, res[char]
-def roman_numerals(N, num, dic, res):
-    if num > N:
-        return
-    temp = []
-    t = -1
-    a = num
-    while num != 0:
-        m = num % 10
-        num /= 10
-        t += 1
-        if m == 1:
-            temp.append(dic[pow(10, t)])
-        if m == 2:
-            temp.append(dic[pow(10, t)] + dic[pow(10, t)])
-        if m == 3:
-            temp.append(dic[pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)])
-        if m == 4:
-            temp.append(dic[pow(10, t)] + dic[5 * pow(10, t)])
-        if m == 5:
-            temp.append(dic[5 * pow(10, t)])
-        if m == 6:
-            temp.append(dic[5 * pow(10, t)] + dic[pow(10, t)])
-        if m == 7:
-            temp.append(dic[5 * pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)])
-        if m == 8:
-            temp.append(dic[5 * pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)] + dic[pow(10, t)])
-        if m == 9:
-            temp.append(dic[pow(10, t)] + dic[pow(10, (t + 1))])
-    for p in temp:
-        for s in p:
-            if s not in res:
-                res[s] = 1
-            else:
-                res[s] += 1
-    roman_numerals(N, a + 1, dic, res)
 if __name__ == "__main__":
     solution()
     
